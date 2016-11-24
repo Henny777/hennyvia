f833a7ac6c58363b370b0ea44da3c596d3a3a06b
Henny777 <jengstrom27@gmail.com>
2016-11-24 20:41:43 +0100

    First commit

---
+++ johnnyvideopage/Controllers/ApiCallsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace johnnyvideopage.Controllers
+{
+    public class ApiCallsController : Controller
+    {
+        // GET: ApiCalls
+        public void IMDBCall()
+        {
+            string path = "http://www.omdbapi.com/?t=Titanic";
+            
+        }
+    }
+}
---
+++ johnnyvideopage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="johnnyvideopage.MvcApplication" Language="C#" %>
